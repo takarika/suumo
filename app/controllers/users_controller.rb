253db94b77181e43d4a2f6ac0b7796f90a4d1a9a
takarika <tkrk0221@gmail.com>
2016-01-11 13:04:51 +0900

    お気に入りした画像をマイページで見れるようにしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-  def index
-     @user = User.all
-  end
-
-
   def show
      @user = User.find(params[:id])
-  end
+     house_ids = @user.favorites.pluck(:house_id)
+     p house_ids
 
+     @favorite_houses = House.find(house_ids)
+
+     # @favorites = Favorite.where({user_id: params[:id]})
+     # @favorites.houses
+
+     p @favorite_houses
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+
   root to: "houses#index"
 
   resources :houses do
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     delete :unfavorite
   end
 
+  devise_for :users
   resources :users, only: [:show]
-
 end
